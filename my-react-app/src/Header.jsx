8755ef3ffd3e0d0968abe9ce2bdaa43b3468c93e
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-30 00:00:29 +0530

    some changes

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>My website </h1>
+            <nav>
+                <Ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">contact</a></li>
+                </Ul>
+            </nav>
+        </header>
+    );
+    
+    
+    }
+    
+    export default Header
