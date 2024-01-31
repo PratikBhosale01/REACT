bdfce4188eaafe255992e67bd04b8a3366367622
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-31 20:29:00 +0530

    card compo

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,13 +1,14 @@
 import AppHeader from './AppHeader.jsx'
 import AppFooter from './AppFooter.jsx'
 import AppFood from './AppFood.jsx'
-
+import AppCard from './AppCard.jsx'
 function App() {
  
   return(
     <>
     <AppHeader/>
     <AppFood/>
+    <AppCard/>
     <AppFooter/>
     
     </>
---
+++ my-react-project/src/AppCard.jsx
@@ -0,0 +1,15 @@
+
+function AppCard (){
+
+    return (
+        <div  className="card">
+            <img></img>
+            <h2>Xplorer</h2>
+            <p>I am web developer and I play games</p>
+        </div>
+    )
+
+}
+
+
+export default AppCard 
