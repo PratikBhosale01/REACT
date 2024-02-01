d248a755b51827758f171724a5b55d36320498f9
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-01 23:43:26 +0530

    css

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -2,6 +2,7 @@ import AppHeader from './AppHeader.jsx'
 import AppFooter from './AppFooter.jsx'
 import AppFood from './AppFood.jsx'
 import AppCard from './AppCard.jsx'
+import Button from './Button/button.jsx'
 function App() {
  
   return(
@@ -9,7 +10,9 @@ function App() {
     <AppHeader/>
     <AppFood/>
     <AppCard/>
+    <Button/>
     <AppFooter/>
+    
     
     </>
   );
---
+++ my-react-project/src/Button/button.jsx
@@ -0,0 +1,9 @@
+import styles from './button.module.css';
+
+function button(){
+    return( <button>click Me</button> )
+        
+    
+ }
+
+ export default button
