afb6fa1b19c369e285618277aa944c28aaffcddd
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-02 23:53:44 +0530

    inline css

--- my-react-project/src/Button/button.jsx
+++ my-react-project/src/Button/button.jsx
@@ -1,9 +1,27 @@
-import styles from './button.module.css';
+// import styles from './button.module.css';
 
 function button(){
-    return( <button>click Me</button> )
+    const styles ={
+    borderRadius: "8px",
+    border: "1px solid transparent",
+    padding: "0.6em 1.2em",
+    fontSize: "1em",
+    fontWeight: "500",
+    fontFamily: "inherit",
+    backgroundColor: "#dcd1d1",
+    cursor: "pointer",
+    transition: "border-color 0.25s",
+
+    }
+
+    return( 
+    //<button className={styles.button} >click Me</button>  
+    <button style={styles} >click Me</button>  //inline css 
+     )
         
-    
+
+    // used curly brackets for dynamic value
+     
  }
 
  export default button
