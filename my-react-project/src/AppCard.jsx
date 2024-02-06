d628344195295f494a2c61e713071efa8224db31
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-06 23:12:09 +0530

    use iframe

--- my-react-project/src/AppCard.jsx
+++ my-react-project/src/AppCard.jsx
@@ -2,10 +2,14 @@
 function AppCard (){
 
     return (
+        <div>
         <div  className="card">
             <img className="card-img" src="http://via.placeholder.com/150" alt="profile pic "></img>
             <h2 className="card-title">Xplorer</h2>
             <p className="card-text">I am web developer and I play games</p>
+        </div>
+        <iframe scrolling="no" class="w-300" height="450" src="https://www.instagram.com/hruta12/embed"></iframe>
+        <iframe class="s_map_embedded o_not_editable" src="https://maps.google.com/maps?q=250%20Executive%20Park%20Blvd%2C%20Suite%203400%20San%20Francisco%20California%20(US)%20United%20States&amp;t=m&amp;z=12&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
         </div>
     )
 
