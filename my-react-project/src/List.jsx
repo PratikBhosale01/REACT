a73ec98dd973b3f7f4a0dff8c2e9daf42838a520
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-07 23:58:03 +0530

    maps

--- my-react-project/src/List.jsx
+++ my-react-project/src/List.jsx
@@ -4,6 +4,7 @@ function List(){
   const fruit = ["apple", "orange", "cherry", "mango"];
 
     const listItems =fruit.map(fruit => <li>{fruit}</li>);
+    // map :-Creates a new array with the results of some operation. The value its callback returns are used to form new array
   return(
       <ol>{listItems}</ol>
     );
