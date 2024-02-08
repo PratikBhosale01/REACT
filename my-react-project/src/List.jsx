999895b96facf232ee26e4d1b14513e44b25e32a
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-08 23:27:18 +0530

    rendering array of object

--- my-react-project/src/List.jsx
+++ my-react-project/src/List.jsx
@@ -1,9 +1,15 @@
 
 function List(){
     
-  const fruit = ["apple", "orange", "cherry", "mango"];
+  const fruit = [{ id:1,name :"apple",cal:12}, 
+                {id:2,name:"orange", cal:13}, 
+                {id:3, name:"cherry", cal:14}, 
+               {id:4, name:"mango" ,cal:15}];
 
-    const listItems =fruit.map(fruit => <li>{fruit}</li>);
+              //  fruit.sort((a,b)=>a.name.localeCompare(b.name));   // aphabatical
+               fruit.sort((a,b)=>b.name.localeCompare(a.name));   //reverse apha
+
+    const listItems =fruit.map(fruit => <li key={fruit.id} >{fruit.name} {fruit.cal}</li>);
     // map :-Creates a new array with the results of some operation. The value its callback returns are used to form new array
   return(
       <ol>{listItems}</ol>
