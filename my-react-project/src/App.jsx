0c57ab6500a7573ac1f09f99a2ad13a753cc8021
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-11 23:07:38 +0530

    short circuting

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -10,17 +10,27 @@ import List from './List.jsx'
 function App() {
 
 
-  const fruit = [{ id:1,name :"apple",cal:12}, 
+  const fruit = [
+    { id:1,name :"apple",cal:12}, 
   {id:2,name:"orange", cal:18}, 
   {id:3, name:"cherry", cal:14}, 
- {id:4, name:"mango" ,cal:15}];
+ {id:4, name:"mango" ,cal:15}
+];
+
+
+const newFruit = [
+ 
+];
  
   return(
     <>
     
     <AppHeader/>
     <UserGreet isLoggedIn={true} username="Xplorer" />
-    <List list={fruit} category="fruit"/>
+   { fruit.length>0? <List list={fruit} category="fruit"/>:null}  
+   {/* if there is no fruit in the list dont render anything */}
+   { newFruit.length>0 && <List list={fruit} category=" new fruit"/> }  
+   {/* this is know as short circuiting*/}
     <AppFood/>
     <Student name="stud"  age={30}  isStudent={true}/>
     <Student/>
