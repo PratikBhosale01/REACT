1d2061889dc6f4b55553de763dd147b59cdca0bd
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-02-12 23:36:41 +0530

    onclick event

--- my-react-project/src/Button/button.jsx
+++ my-react-project/src/Button/button.jsx
@@ -13,11 +13,29 @@ function button(){
     transition: "border-color 0.25s",
 
     }
+        // click event = An interaction when a user clicks on a Epecific element.
+    //           we can respond to clicks by passing a callback to the onclick event handler.
+
+    const clickMe = () => { console.log("Oouch !");};
+    const stopClick = (name) => { console.log(` ${name} stopp clicking me ... !!!`); };
+
+
+      
+
 
     return( 
-    //<button className={styles.button} >click Me</button>  //modules css
-    <button style={styles} >click Me</button>  //inline css 
-     )
+        //<button className={styles.button} >click Me</button>  //modules css 
+      //<button style={styles} onClick={clickMe} >click Me</button>  //inline css 
+
+    
+    <>
+   
+    <button style={styles} onClick={clickMe} >click Me</button>
+    <button style={styles} onClick={()=>stopClick("bro")} >click Me pls</button>  
+     </>
+
+     //used arrow function on line 34 because function call with paranthesis will execute it immediately only for line 34 case 
+    )
         
 
     // used curly brackets for dynamic value
